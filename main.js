1a69ad601972d883b0c5e6572eb05cbcae595c02
Ranjith <ranjith.19it@kct.ac.in>
2022-01-15 01:41:01 +0530

    donar html page completed

---
+++ main.js
@@ -0,0 +1,3 @@
+const phoneInputField = document.querySelector("#donor-contact-number");
+// const phoneInputField = document.querySelector("#donor-Whatsapp-No");
+const phoneInput = window.intlTelInput(phoneInputField, {});
