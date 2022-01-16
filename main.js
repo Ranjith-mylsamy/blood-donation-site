13278560a00a5919df38fd28e0e4dcc3fc87ec60
Ranjith <ranjith.19it@kct.ac.in>
2022-01-16 20:30:46 +0530

    bug in javascript section

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 const phoneInputField = document.querySelector("#donor-contact-number");
 // const phoneInputField = document.querySelector("#donor-Whatsapp-No");
 const phoneInput = window.intlTelInput(phoneInputField, {});
+const formBloodType = document.querySelector(".form--blood-type");
+const formPveBtn = document.querySelector(".form--button-positive");
+const formNveBtn = document.querySelector(".form--button-negative");
+
+
+formNveBtn.addEventListener("click", ()=> {
+    formBloodType.setAttribute("value","negative");
+    formNveBtn.style.backgroundColor = "#FF0000";
+    formNveBtn.style.color="#fff";
+    formPveBtn.style.backgroundColor = "#fff";
+    formPveBtn.style.color="#746868";
+});
+
+formPveBtn.addEventListener("click", ()=> {
+    formBloodType.setAttribute("value","positive");
+    formPveBtn.style.backgroundColor = "#FF0000";
+    formPveBtn.style.color="#fff";
+    formNveBtn.style.backgroundColor = "#fff";
+    formNveBtn.style.color="#746868";
+});
